88af465077721b2564cd03de7be8eaf59e358ae5
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-09-11 13:51:48 +0300

    Добавил необходимый метод поиска юзера по машине

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -19,11 +19,23 @@ public class UserDaoImp implements UserDao {
       sessionFactory.getCurrentSession().save(user);
    }
 
-   @Override
+
    @SuppressWarnings("unchecked")
+   @Override
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public User findByCar(String model, int series) {
+      TypedQuery<User> query = sessionFactory
+              .getCurrentSession()
+              .createQuery("FROM User user JOIN FETCH user.car WHERE user.car.model = :model AND user.car.series = :series")
+              .setParameter("model", model)
+              .setParameter("series", series);
+      return query.getSingleResult();
    }
 
 }
