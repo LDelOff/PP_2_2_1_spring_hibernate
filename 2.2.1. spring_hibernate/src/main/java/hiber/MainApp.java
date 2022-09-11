d06837903e768b07af9855f5420b3a9cfc24d016
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-09-11 13:49:38 +0300

    Переделал под условие задачи

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,25 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Audi", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Nissan", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Toyota", 4)));
 
+
+      System.out.println("Список людей:");
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car = " + user.getCar());
          System.out.println();
       }
+
+      System.out.println("Поиск человека по машине:");
+      System.out.println(userService.findByCar("Nissan", 3));
 
       context.close();
    }
